4ba6f6592660b37e3ff48ac76d22ccda4dcb99ae
Abel Bokure <abel.ngt@gmail.com>
2022-04-04 19:50:44 -0400

    commit tree 4bc77ca62e415d43d2afafef1b5734b7e7ecbeb2

---
+++ index.js
@@ -0,0 +1,16 @@
+1 + 80; //=> 81
+60 - 40; //=> 20
+2 * 3.4; //=> 6.8
+5.0 / 2.5; //=> 2
+function add(a,b) { return a + b
+    // we'll fill this in shortly
+  }
+  function subtract(a,b) { return a - b
+    // we'll fill this in shortly
+  }
+  function multiply(a,b) { return a * b
+    // we'll fill this in shortly
+  }
+  function divide(a,b) { return a / b
+    // we'll fill this in shortly
+  }
